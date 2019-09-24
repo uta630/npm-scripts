e761ea60ed88d3f78fab9fa2ce5db5b8897a081d
臼井健志 <ta-usui@media-plex.net>
2019-09-24 18:24:29 +0900

    gulpfileの記述をバージョンアップ

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,110 @@
+/*
+ * plugin
+ */
 // require : settings
-const gulp = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp')
 const packageImporter = require('node-sass-package-importer');
-const rename = require("gulp-rename");
+const $ = require('gulp-load-plugins')();
 
 // require : scss
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
 const autoprefixer = require('autoprefixer');
 const cssdeclsort = require('css-declaration-sorter');
-const mmq = require('gulp-merge-media-queries');
-const postcss = require('gulp-postcss');
-
-// require : css
-const gulpStylelint = require('gulp-stylelint');
-const header = require('gulp-header');
-const cleanCSS = require('gulp-clean-css');
-
-// require : html
-const ejs = require('gulp-ejs');
-const htmlmin = require('gulp-htmlmin');
 
 // require : js
-const uglify = require('gulp-uglify');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 
 // require : images
-const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const mozjpeg = require('imagemin-mozjpeg');
 
 // require : ローカルサーバー
 const browserSync = require('browser-sync').create();
 
-// task : scss
-gulp.task('scss', function() {
-  return gulp
-    .src( './src/scss/style.scss' )
-    .pipe( plumber({ errorHandler: notify.onError( 'Error: <%= error.message %>' ) }) )
-    .pipe( sassGlob() )
-    .pipe( sass({
+/*
+ * env
+ */
+const minimist = require('minimist');
+const envOption = {
+  string: 'env',
+  default: {
+    env: process.env.NODE_ENV || 'development', // NODE_ENVに指定がなければ開発モードをデフォルトにする
+  }
+};
+const options = minimist(process.argv.slice(2), envOption);
+const isProduction = (options.env === 'production') ? true : false;
+console.log('[build env]', options.env, '[is production]', isProduction);
+
+const argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+
+/*
+ * config
+ */
+const config = {
+  development: { // 開発用
+    root: 'dist',
+    path: {
+      absolute: 'http://localhost:3000',
+      relative: '/',
+    }
+  },
+  production : { // 公開用
+    root: 'prod',
+    path: {
+      absolute: 'https://www.example.com',
+      relative: '/',
+    }
+  }
+};
+
+/*
+ * tasks
+ */
+const TASK__scss = () => {
+  return src( './src/scss/style.scss' )
+    .pipe( $.plumber({ errorHandler: $.notify.onError( 'Error: <%= error.message %>' ) }) )
+    .pipe( $.sassGlob() )
+    .pipe( $.sass({
       outputStyle: 'expanded',
       importer: packageImporter({
           extensions: ['.scss', '.css']
       }),
       includePaths: require('node-reset-scss').includePath
     }) )
-    .pipe( postcss([ autoprefixer() ]) )
-    .pipe( postcss([ cssdeclsort({ order: 'alphabetically' }) ]) )
-    .pipe( mmq() )
-    .pipe( gulp.dest( 'dist/css' ) );
-});
+    .pipe( $.postcss([ autoprefixer() ]) )
+    .pipe( $.postcss([ cssdeclsort({ order: 'alphabetically' }) ]) )
+    .pipe( $.mergeMediaQueries() )
+    .pipe( dest( `${config[options.env].root}/css` ) );
+};
 
-// task : css
-gulp.task('css', function(){
-  return gulp
-    .src( './dist/css/style.css' )
-    .pipe( header('@charset "utf-8";\n') )
-    .pipe( cleanCSS() )
-    .pipe( rename({ extname: '.min.css' }) )
-    .pipe( gulp.dest( 'dist/css' ) );
-});
+const TASK__css = () => {
+  return src(`./${config[options.env].root}/css/style.css`)
+    .pipe( $.header('@charset "utf-8";\n') )
+    .pipe( $.cleanCss() )
+    .pipe( $.rename({ extname: '.min.css' }) )
+    .pipe( dest( `${config[options.env].root}/css` ) );
+};
 
-// task : html
-gulp.task('ejs', function(){
-  return gulp
-    .src(['./src/ejs/*.ejs'])
-    .pipe(plumber({
-        handleError: function(err){
+const TASK__ejs = () => {
+  return src('./src/ejs/**/[^_]*.ejs')
+    .pipe($.plumber({
+        handleError: (err) => {
             this.emit('end');
         }
     }))
-    .pipe(ejs())
-    .pipe(rename({extname: '.html'}))
-    .pipe(gulp.dest('./src/html'));
-});
-gulp.task('minify-html', function(){
-  return gulp
-    .src('src/html/*.html')
-    .pipe(htmlmin({
-        collapseWhitespace : true,
-        removeComments : true
+    .pipe($.data(file => {
+      const path = config[options.env].path
+      return { path }
     }))
-    .pipe(gulp.dest('dist'))
-});
+    .pipe($.ejs())
+    .pipe($.rename({extname: '.html'}))
+    .pipe(dest(`./${config[options.env].root}`));
+};
 
-// task : images
-gulp.task('imagemin', function () {
-  return gulp
-    .src('src/images/*.{jpg,jpeg,png,gif,svg}')
-    .pipe(imagemin([
+const TASK__imagemin = () => {
+  return src('src/images/*.{jpg,jpeg,png,gif,svg}')
+    .pipe($.imagemin([
       pngquant({
         quality: [.65, .85],
         speed: 1
@@ -101,57 +114,68 @@ gulp.task('imagemin', function () {
         progressive: true
       })
     ]))
-    .pipe(gulp.dest('dist/images'));
-});
+    .pipe(dest(`${config[options.env].root}/images`));
+};
 
-// task : webpack
-gulp.task('webpack',function(){
+const TASK__webpack = () => {
   return webpackStream({
     entry: './src/js/script.js',
       output: {
         filename: 'script.js'
       }
     }, webpack)
-    .pipe(gulp.dest('dist/js'));
-});
-// task : js minify
-gulp.task('minjs', function() {
-  return gulp.src("dist/js/script.js")
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/js/'));
-});
+    .pipe($.babel())
+    .pipe(dest(`${config[options.env].root}/js`));
+};
+const TASK__minjs = () => {
+  return src(`${config[options.env].root}/js/script.js`)
+    .pipe($.uglify())
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe(dest(`${config[options.env].root}/js/`));
+};
+const TASK__json = () => {
+  return src('./src/data/**/*.json')
+    .pipe($.jsonminify())
+    .pipe(dest(`${config[options.env].root}/data/`));
+};
 
-// ローカルサーバーの立ち上げ
-// https://teratail.com/questions/168814
+/*
+ * browserSync
+ */
 const browserSyncOption = {
   server: {
-    baseDir: './dist/'
+    baseDir: config[options.env].root
   },
   reloadOnRestart: true
 };
-
-function sync(done) {
+const sync = done => {
   browserSync.init(browserSyncOption);
   done();
 }
-
-// watch&リロード 処理
-function watchFiles(done) {
+const watchFiles = done => {
   const browserReload = () => {
     browserSync.reload();
     done();
   };
-  gulp.watch('src/scss/**/*.scss').on('change', gulp.series('scss', 'css', 'imagemin', browserReload));
-  gulp.watch('src/**/*.ejs').on('change', gulp.series('ejs', 'minify-html', 'imagemin', browserReload));
-  gulp.watch('src/**/*.js').on('change', gulp.series('webpack', 'minjs', 'imagemin', browserReload));
+  watch('src/scss/**/*.scss').on('change', series(TASK__scss, TASK__css, TASK__imagemin, browserReload));
+  watch('src/ejs/**/*.ejs').on('change', series(TASK__ejs, TASK__imagemin, browserReload));
+  watch('src/js/**/*.js').on('change', series(TASK__webpack, TASK__minjs, TASK__imagemin, browserReload));
+  watch('src/data/**/*.json').on('change', series(TASK__json, browserReload));
 }
 
-gulp.task('default',
-  gulp.series(
-    'scss', 'css',
-    'ejs', 'minify-html',
-    'webpack', 'minjs',
-    'imagemin',
-    sync, watchFiles)
+/*
+ * scripts
+ */
+exports.default = series(
+  TASK__scss, TASK__css,
+  TASK__ejs,
+  TASK__webpack, TASK__minjs,
+  TASK__json, TASK__imagemin,
+);
+exports.watch = series(
+  TASK__scss, TASK__css,
+  TASK__ejs,
+  TASK__webpack, TASK__minjs,
+  TASK__json, TASK__imagemin,
+  sync, watchFiles
 );
